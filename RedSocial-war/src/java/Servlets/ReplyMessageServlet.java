5ce314009c3abd94eeaad54822bbba9bfe990e93
torino09 <rlillo.815@gmail.com>
2019-05-09 20:28:58 +0200

    Reply message done

---
+++ RedSocial-war/src/java/Servlets/ReplyMessageServlet.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Servlets;
+
+import RedSocialEntities.Users;
+import RedSocialFacades.UsersFacade;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ak
+ */
+@WebServlet(name = "ReplyMessageServlet", urlPatterns = {"/ReplyMessageServlet"})
+public class ReplyMessageServlet extends HttpServlet {
+
+    @EJB
+    private UsersFacade usersFacade;
+
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String title = request.getParameter("messageTitle");
+        Integer id = Integer.parseInt(request.getParameter("messageSender"));
+        Users receiver = usersFacade.find(id);
+        
+        request.setAttribute("receiver", receiver);
+        request.setAttribute("title", title);
+        
+        RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/ReplyMessage.jsp");
+        rd.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ RedSocial-war/web/UserInbox.jsp
@@ -0,0 +1,69 @@
+<%-- 
+    Document   : UserChat
+    Created on : 07-may-2019, 20:30:44
+    Author     : Alae Akalay
+--%>
+
+<%@page import="RedSocialEntities.Messages"%>
+<%@page import="java.util.List"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<%
+    List<Messages> inbox = (List) request.getAttribute("listaEntrantes");
+%>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>INBOX</title>
+        <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h2>Inbox:</h2>
+
+        <table>
+            <tr> 
+                <th>Tittle</th>
+                <th>Date:</th>
+                <th>From:</th>
+                <th>Message:</th>
+            </tr>
+            <%
+                for (Messages m : inbox) {
+            %>
+            <tr>
+                <td><%= m.getTitle()%></td>
+                <td><%= m.getDate().toString()%></td>
+                <td><%= m.getSender().getName()%></td>
+                <td><%= m.getText()%></td>
+                <td>
+                    <form action="ReplyMessageServlet">
+                        <input type="hidden" name="messageSender" value="<%= m.getSender().getId()%>">
+                        <input type="hidden" name="messageTitle" value="<%= m.getTitle()%>">
+                        <button>Reply</button>
+                    </form>
+                </td>
+            </tr>
+            <%
+                }
+            %>
+            <a href="UserOutboxServlet">Check Outbox</a>
+            <a href="">Create new message.</a>
+        </table>
+    </body> 
+</html>
